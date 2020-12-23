51d4d2c6db2bd1f257705fff1bd1f6504e3e1162
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-12-23 05:53:54 +0545

    route params

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/api/courses', (req, res) => {
   res.send([1, 2, 3]);
 });
 
-const PORT = 3000;
+app.get('/api/courses/:id', (req, res) => res.send(req.params.id));
+
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}...`));
