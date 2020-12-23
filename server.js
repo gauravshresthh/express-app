1b49c2e71a98bc539ebf1daa027e9cd19a2249b2
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-12-23 05:59:17 +0545

    courses finding

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { id: 1, name: 'React' },
+  { id: 2, name: 'Node' },
+  { id: 3, name: 'mongoDB' },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello world from express');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.get('/api/courses/:id', (req, res) => res.send(req.params.id));
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
 
+  if (!course) {
+    res.status(404).send('Course with the provided ID was not found');
+  }
+  res.send(course);
+});
 
 const PORT = process.env.PORT || 3000;
 
